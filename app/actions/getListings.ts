e7688d83389c35d8bb3d509152900485b9dfb067
thebasedtaka <darktaka9998@gmail.com>
2024-03-30 01:27:21 -0400

    Interface change

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,6 +2,13 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 export default async function getListings(params: IListingParams) {
   try {
@@ -14,10 +21,6 @@ export default async function getListings(params: IListingParams) {
       category,
       startDate,
       endDate,
-      price,
-      orderBy,
-      search,
-      type,
     } = params;
 
     let query: any = {};
